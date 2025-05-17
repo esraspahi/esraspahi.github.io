4af6923390f01ebdacfd10eac606b5747c75e313
esraspahi <64081156+esraspahi@users.noreply.github.com>
2025-05-17 20:30:55 +0000

    Deploying to gh-pages from @ esraspahi/esraspahi.github.io@7479343662e6e95d64fe4b425284aea6b06a182b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,13 +511,6 @@ ninja.data = [{
           window.open("https://github.com/esraspahi", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
@@ -532,25 +525,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
         id: 'social-x',
         title: 'X',
         section: 'Socials',
         handler: () => {
           window.open("https://twitter.com/esraspahi", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://esraspahi.github.io", "_blank");
         },
       },{
       id: 'light-theme',
